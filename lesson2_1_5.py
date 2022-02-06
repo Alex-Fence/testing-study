be5e4f98b058897d0ba0f504fdeff7e08ef03e1b
alex <bokuto@mail.ru>
2022-02-06 09:49:46 +0300

    урок 2.1 шаг 5 начало

--- lesson2_1_5.py
+++ lesson2_1_5.py
@@ -3,9 +3,11 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import math
 
-def calc(x):
-    return str(math.log(abs(12*math.sin(int(x)))))
 
-link ="http://suninjuly.github.io/math.html"
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+link = "http://suninjuly.github.io/math.html"
 browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-x = browser.find_element(By.ID,'input_value')
+x = browser.find_element(By.ID, 'input_value')
